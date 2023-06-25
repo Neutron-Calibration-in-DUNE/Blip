4cfb76d7ba3b86e6e9e404874271b70a162165cf
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-06-25 09:00:37 -0700

    added 'load_config' option to configs with nested heirarchies (kind of like LArSoft fcls)

--- blip/utils/config.py
+++ blip/utils/config.py
@@ -15,6 +15,31 @@ class ConfigParser:
     def __init__(self, 
         config_file:    str,
     ):
+        self.logger = Logger('config_parser', output="both", file_mode='w')
+        self.logger.info(f"setting up config file.")
         self.config_file = config_file
-        with open(self.config_file, 'r') as file:
-            self.data = yaml.safe_load(file)
+        self.nested_config_files = [config_file]
+        self.nested_configs = []
+        self.data = {}
+        self.parse_config()
+
+    def parse_config(self): 
+        self.collect_nested_configs(self.config_file)
+        self.nested_config_files.reverse()
+        self.nested_configs.reverse()
+        for config in self.nested_configs:
+            self.data.update(config)
+        self.logger.info(
+            f'parsed {len(self.nested_configs)} files with the heirarchy: ' + 
+            f'{self.nested_config_files}.'
+        )
+
+    def collect_nested_configs(self,
+        config_file
+    ):
+        with open(config_file, 'r') as file:
+            temp_data = yaml.safe_load(file)
+            self.nested_configs.append(temp_data)
+        if 'load_config' in temp_data.keys():
+            self.nested_config_files.append(temp_data['load_config'])
+            self.collect_nested_configs(temp_data['load_config'])
