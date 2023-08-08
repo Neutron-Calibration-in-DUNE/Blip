60265e527dd129820122d6d53fc80c08f720aabc
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-08-08 07:58:40 -0700

    added arrakis program

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
     # possible entry point
     entry_points={
         'console_scripts': [
+            'arrakis = blip.programs.run_arrakis:run',
             'blip = blip.programs.run_blip:run',
             'blip_display = blip.programs.run_blip_server:run'
         ],
