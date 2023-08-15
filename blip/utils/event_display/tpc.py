33dc02fcaf7bbb6b4cd822f0b0dc23a29f42c5f6
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-08-15 09:24:08 -0700

    blip display

--- blip/utils/event_display/tpc.py
+++ blip/utils/event_display/tpc.py
@@ -539,8 +539,7 @@ class TPCDisplay:
     functions here are for updating the Wire Plane display plots.
     """
     def update_first_figure_adc_slider_option(self, attr, old, new):
-        print(self.first_figure_adc_slider_option.active)
-        if attr == [0]:
+        if 0 in self.first_figure_adc_slider_option.active:
             self.first_figure_adc_slider_option_bool = True
         else:
             self.first_figure_adc_slider_option_bool = False
