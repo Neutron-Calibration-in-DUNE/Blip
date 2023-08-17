1d3b6756115db1abf1ccbb1bfec33605850a675d
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-08-17 09:34:36 -0700

    fixing things

--- blip/dataset/blip.py
+++ blip/dataset/blip.py
@@ -192,9 +192,11 @@ class BlipDataset(InMemoryDataset, GenericDataset):
                 'view_2_source_points', 'view_2_topology_points', 'view_2_particle_points', 'view_2_physics_points', 'view_2_total_points',
             ]:
                 if point_label in temp_arrakis_meta[ii+1].keys():
-                    print(self.meta[point_label])
-                    for key in self.meta[point_label].keys():
-                        self.meta[point_label][key] += temp_arrakis_meta[ii+1][point_label][key]
+                    if 'total' in point_label:
+                        self.meta[point_label] += temp_arrakis_meta[ii+1][point_label]
+                    else:
+                        for key in self.meta[point_label].keys():
+                            self.meta[point_label][key] += temp_arrakis_meta[ii+1][point_label][key]
 
         # arange dictionaries for label<->value<->index maps
         self.meta['edep_features_names'] = list(self.meta['edep_features'].keys())
