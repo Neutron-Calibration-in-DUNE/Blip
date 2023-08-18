7bb9925c27b932216df03e79ae8efef5ab7b6464
Nicholas Carrara <ncarrara.physics@gmail.com>
2023-08-18 10:52:36 -0700

    fixing things

--- blip/dataset/blip.py
+++ blip/dataset/blip.py
@@ -617,6 +617,7 @@ class BlipDataset(InMemoryDataset, GenericDataset):
             for key, val in self.meta['blip_labels_values_map'][classes].items():
                 event_classes[:, class_index][(event_classes[:, class_index] == key)] = val
         event_classes = event_classes[:, self.meta['blip_classes_indices']]
+        print("after again", np.unique(event_classes))
         event_clusters = event_clusters[:, self.meta['blip_clusters_indices']]
         event_hits = event_hits[:, self.meta['blip_hits_indices']]
 
