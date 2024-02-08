237daaa5c1a86404565ccf23ada79edd913e32a6
Marjolein van Nuland - Troost <mnuland@login05.chn.perlmutter.nersc.gov>
2024-02-08 04:33:41 -0800

    Add parameter dictionary

--- blip/models/blip_light.py
+++ blip/models/blip_light.py
@@ -6,34 +6,45 @@ import torch.nn as nn
 
 from blip.models import GenericModel
 
+blip_light_params = {
+    "dropout": 0.2,
+    "kernel_size": 2,
+    "n_filters": 32,
+}
+
 class CNNLivio(nn.Module):
     """
     Basically you want for each flash to fill a (2,24) array with the intensity of each SiPM as input and then extract the center
     of gravity of the charge hits as truth variable
     """
-    def __init__(self, dropout=0.2, kernel_size = 2,n_filters=32):
+    def __init__(
+            self,
+            config: dict = blip_light_params,
+    ):
         super(CNNLivio, self).__init__()
-        self.n_filters = n_filters
-        self.padding = kernel_size // 2
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=n_filters, kernel_size=kernel_size, stride=1, padding=self.padding)
+        self.n_filters = config["n_filters"]
+        self.dropout = config["dropout"]
+        self.kernel_size = config["kernel_size"]
+        self.padding = self.kernel_size // 2
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=self.n_filters, kernel_size=self.kernel_size, stride=1, padding=self.padding)
         self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
 
-        self.conv2 = nn.Conv2d(in_channels=n_filters, out_channels=n_filters*2, kernel_size=3, stride=1, padding=self.padding)
+        self.conv2 = nn.Conv2d(in_channels=self.n_filters, out_channels=self.n_filters*2, kernel_size=3, stride=1, padding=self.padding)
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
 
-        self.conv3 = nn.Conv2d(in_channels=n_filters*2, out_channels=n_filters*4, kernel_size=3, stride=1, padding=self.padding)
+        self.conv3 = nn.Conv2d(in_channels=self.n_filters*2, out_channels=self.n_filters*4, kernel_size=3, stride=1, padding=self.padding)
         self.relu3 = nn.ReLU()
         self.pool3 = nn.MaxPool2d(kernel_size=(1, 1), stride=(2, 2))
 
-        self.fc1 = nn.Linear(in_features=n_filters*4 * 1 * 3, out_features=4096)
+        self.fc1 = nn.Linear(in_features=self.n_filters*4 * 1 * 3, out_features=4096)
         self.relu4 = nn.ReLU()
-        self.dropout1 = nn.Dropout(p=dropout)
+        self.dropout1 = nn.Dropout(p=self.dropout)
 
         self.fc2 = nn.Linear(in_features=4096, out_features=1024)
         self.relu5 = nn.ReLU()
-        self.dropout2 = nn.Dropout(p=dropout)
+        self.dropout2 = nn.Dropout(p=self.dropout)
 
         self.fc3 = nn.Linear(in_features=1024, out_features=2)
 
